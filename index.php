ab1f36ed35569d49bfa0b10000654f651d3e6b27
EmaSerra <emanuela.serra@icloud.com>
2018-10-04 00:44:58 +0100

    testing

---
+++ index.php
@@ -0,0 +1,85 @@
+<!Doctype HTML>
+
+<head>
+<meta charset="UTF-8">
+<title>Demo Page</title>
+<link rel="stylesheet"	type="text/css"	href="assets/css/style.css"	media="screen">
+</head>
+
+<body>
+
+<div class="container clearfix">
+
+	<section class="logo">
+            <img src="assets/images/logo.jpg" width="170" height="150"alt="Logo">
+	</section>
+
+	<nav>
+		<ul>
+			<li><a href="#"	title="Home">Home </a></li>
+			<li><a href="#"	title="About">About </a></li>
+			<li><a href="#"	title="Contact">Contact </a></li>
+		</ul>
+	</nav>
+
+<div class="main-content clearfix">
+
+	<section class="image-blocks cols-2 clearfix">
+		<div>
+                    <img src="assets/images/dolce.jpg"width="630" height="315"	alt="">
+		</div>
+		<div>
+		     <img src="assets/images/salato.jpg"width="630" height="315"	alt="">
+		</div>
+	</section>
+
+	<section class="text-blocks cols-3 clearfix">
+		<div>
+			<h2>Heading</h2>
+			<p>Lorem Ipsum </p>
+			<p>Lorem Ipsum </p>
+		</div>
+		<div>
+			<h2>Heading</h2>
+			<p>Lorem Ipsum </p>
+			<p>Lorem Ipsum </p>
+		</div>
+		<div>
+			<h2>Heading</h2>
+			<p>Lorem Ipsum </p>
+			<p>Lorem Ipsum </p>
+		</div>
+	</section>
+<!--
+	<section class="image-blocks cols-2 clearfix">
+		<div>
+			<img src="http://placehold.it/630x315"	alt="">
+		</div>
+		<div>
+			<img src=http://placehold.it/630x315 	alt="">
+		</div>
+	</section>
+-->
+	<section class="text-blocks cols-1	clearfix">
+		<div>
+			<p>
+			Lorem ipsum
+			</p>
+			<p>
+			Lorem ipsum
+			</p>
+		</div>
+	</section>
+
+	<section class="copyright clearfix">
+		<p> Copywrite @ Company Name.</p>
+	</section>
+
+</div>
+</div>
+
+
+	
+
+</body>
+</html>
